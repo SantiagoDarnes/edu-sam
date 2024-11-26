d020d00d4bd65f4ae878ad75841c58aa4b3afc35
Gonza <gajara@estudiantes.unsam.edu.ar>
2024-11-26 17:53:12 -0300

    add inscripcion a materia

--- app/modules/subject_registration/routes.py
+++ app/modules/subject_registration/routes.py
@@ -3,6 +3,12 @@ from app.modules.subject_registration import bp
 from app.auth import AuthService, login_required, require_profile
 from app.models.student.student_career import StudentCareer
 from app.models.subject import Subject
+from app.models.student.student_subject import StudentSubject
+from app.models.student.student import Student
+from app.models.academic_period import AcademicPeriod
+from app.models.semester import Semester
+from app import db
+# No tengo idea de como usar AcademicPeriod
 
 auth_service = AuthService()
 
@@ -21,6 +27,7 @@ def available_subjects():
     subjects = []
     for career_id in career_id_unique:
         for subj in Subject.query.filter_by(career_id=career_id).all():
+            
             subjects.append({
                 "id":subj.id,
                 "code":subj.code,
@@ -32,3 +39,17 @@ def available_subjects():
     
     return render_template("subject_registration/subject_registration.html", subjects=subjects)
 
+@bp.route("subject_selected/<subject_id>")
+@login_required
+@require_profile("ESTUDIANTE")
+def select_subject(subj):
+    user = auth_service.get_current_user()
+    student_subj = StudentSubject(
+        student_id=Student.query.where(person_id=user.id).first(),
+        subject_id=subj,
+        enrollment_date="2024-11-26",
+        status=1,
+        created_at="2024-11-26 17:32:00.0",
+        updated_at="2024-11-26 17:32:00.0"
+    )
+    # db.add(student_subj)
