2199c8be3bf19e7fc6ab36ba82f04151cc388bf9
Gonza <gajara@estudiantes.unsam.edu.ar>
2024-11-27 19:56:13 -0300

    testing all routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,15 +1,12 @@
 import pytest
 
-def test_login(test_client):
+def test_routes(test_client):
     response = test_client.get("/login/")
     assert response.status_code == 200  # La pagina existe
     
-    # Me redirecciona al login
-    assert test_client.get("/home/student").status_code == 302
-    
     # Test login
     response = test_client.post("/login/", data={"username":"sdarnes", "password":"44011774"})
-    assert response.status_code == 302  # Al ingresar, me redirecciona. Es un error en realidad
+    assert response.status_code == 302  # Al ingresar, me redirecciona
     response = test_client.get("/home/student")
     assert response.status_code == 200  # Me deja ingresar a la pagina principal despues del login
     
@@ -21,10 +18,43 @@ def test_login(test_client):
     response = test_client.get("/exam_registration/")
     assert response.status_code == 200
     
+    # Test de reportes
+    response = test_client.get("/reports/")
+    assert response.status_code == 200
+    
+    # Test de trámites
+    response = test_client.get("/procedures/")
+    assert response.status_code == 200
+    
+    # Test de datos personales
+    response = test_client.get("/personal_data/")
+    assert response.status_code == 200
+    
     # Test logout
     response = test_client.get("/login/logout")
-    assert response.status_code == 302
+    assert response.status_code == 302  # Me redirecciona de vuelta al login
 
+
+def test_redirect(test_client):
+    # Me redirecciona al login
+    assert test_client.get("/home/student").status_code == 302
     
-
-
+    # Test registros de materias
+    response = test_client.get("/subject_registration/")
+    assert response.status_code == 302
+    
+    # Test registros de examenes
+    response = test_client.get("/exam_registration/")
+    assert response.status_code == 302
+    
+    # Test de reportes
+    response = test_client.get("/reports/")
+    assert response.status_code == 302
+    
+    # Test de trámites
+    response = test_client.get("/procedures/")
+    assert response.status_code == 302
+    
+    # Test de datos personales
+    response = test_client.get("/personal_data/")
+    assert response.status_code == 302
