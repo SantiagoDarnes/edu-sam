36c3ecdfe806b787a8afbe80ab7b9b5ecb14fbc8
Santiago Darnes <santi.darnes@gmail.com>
2024-11-28 13:43:20 -0300

    Switch profiles

--- app/modules/home/routes.py
+++ app/modules/home/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, redirect, url_for
+from app.extensions import db
 from app.modules.home import bp
 from app.auth import AuthService, login_required, require_profile
 from app.models.academic_period import AcademicPeriod
+from app.models.profile import Profile
 from app.qr_factory import QRCodeFactory, send_file
 
 auth_service = AuthService()
@@ -12,6 +14,8 @@ auth_service = AuthService()
 def home_student():
     user = auth_service.get_current_user()
     username = user.first_name.title() + ' ' + user.last_name.title()
+    default_profile = Profile.get_default_profile(user.id)
+    profiles = sorted(user.profiles, key=lambda profile: profile.id != user.default_profile)
 
     academic_period = AcademicPeriod.get_current()
     period_data = {
@@ -21,7 +25,24 @@ def home_student():
         'inscription_end_date': academic_period.inscription_end_date.strftime('%d/%m/%Y'),
     }
 
-    return render_template('student/home.html', username=username, academic_period=period_data )
+    return render_template('student/home.html', username=username, academic_period=period_data, default_profile=default_profile, profiles=profiles)
+
+
+@bp.route("/switch_profile/<int:profile_id>")
+@login_required
+def switch_profile(profile_id):
+    user = auth_service.get_current_user()
+
+    user.default_profile = profile_id
+    db.session.commit()
+
+    # Redirigir según el nuevo perfil
+    if profile_id == 1:
+        return redirect(url_for('home.home_student'))
+    elif profile_id == 2:
+        return redirect(url_for('home.home_professor'))
+    else:
+        return redirect(url_for('home.home_admin'))
 
 
 @bp.route('/academic_period_qr')
@@ -51,5 +72,9 @@ def home_professor():
 def home_admin():
     user = auth_service.get_current_user()
     username = user.first_name.title() + ' ' + user.last_name.title()
-    return render_template('admin/home.html', username=username)
+    default_profile = Profile.get_default_profile(user.id)
+    profiles = sorted(user.profiles, key=lambda profile: profile.id != user.default_profile)
+
+    return render_template('admin/home.html', username=username, default_profile=default_profile, profiles=profiles)
+
     
