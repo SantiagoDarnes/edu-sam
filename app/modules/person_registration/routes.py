44d5d371c6c50aa8fd0bb2531d85e966d0d0f06d
Santiago Darnes <santi.darnes@gmail.com>
2024-11-28 08:59:52 -0300

    Admin - person registration

--- app/modules/person_registration/routes.py
+++ app/modules/person_registration/routes.py
@@ -10,17 +10,10 @@ from app.models.administrator import Administrator
 
 auth_service = AuthService()
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 @login_required
 @require_profile("ADMINISTRADOR")
 def index():
-    return render_template('admin/person_registration.html')
-
-
-@bp.route("/register", methods=["POST"])
-@login_required
-@require_profile("ADMINISTRADOR")
-def register():
     if request.method == 'POST':
         identity_number = request.form['identity_number']
         first_name = request.form['first_name']
@@ -33,33 +26,37 @@ def register():
         try:
             new_person = Person(
                 identity_number=identity_number,
-                first_name=first_name,
-                last_name=last_name,
+                first_name=first_name.upper(),
+                last_name=last_name.upper(),
                 email=email,
                 phone=phone,
                 birth_date=birth_date
             )
             new_person.set_username()
-            new_person.set_password()
+            new_person.generate_password()
             db.session.add(new_person)
             db.session.commit()
 
             if user_type == 'ESTUDIANTE':
-                new_student = Student(person_id=new_person.id)
+                new_student = Student(person_id=new_person.id, admission_date=db.func.current_date())
                 new_student.set_enrollment_number()
                 db.session.add(new_student)
+                new_person.default_profile = 1
             elif user_type == 'PROFESOR':
                 new_professor = Professor(person_id=new_person.id)
                 new_professor.set_professor_code()
                 db.session.add(new_professor)
+                new_person.default_profile = 2
             elif user_type == 'ADMINISTRADOR':
                 new_admin = Administrator(person_id=new_person.id, access_level_id=1)
                 db.session.add(new_admin)
+                new_person.default_profile = 3
 
             db.session.commit()
             flash("Persona registrada exitosamente.", "success")
         except Exception as e:
             db.session.rollback()
             flash(f"Error al registrar la persona: {str(e)}", "danger")
+            print(e)
 
-        return redirect(url_for('person_registration.index'))
+    return render_template('admin/person_registration.html')
