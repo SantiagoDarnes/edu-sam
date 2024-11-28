4cb0ed18e7fdd57578ebe4e6d0306daffdd851be
Santiago Darnes <santi.darnes@gmail.com>
2024-11-27 22:12:47 -0300

    Home redesign and wr factory

--- app/modules/home/routes.py
+++ app/modules/home/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for
 from app.modules.home import bp
 from app.auth import AuthService, login_required, require_profile
 from app.models.academic_period import AcademicPeriod
+from app.qr_factory import QRCodeFactory, send_file
 
 auth_service = AuthService()
 
@@ -21,6 +22,19 @@ def home_student():
     }
 
     return render_template('student/home.html', username=username, academic_period=period_data )
+
+
+@bp.route('/academic_period_qr')
+@login_required
+def academic_period_qr():
+    academic_period = AcademicPeriod.get_current()
+    event_name = f"EduSam - {academic_period.name}"
+    start_date = academic_period.start_date
+    end_date = academic_period.end_date
+
+    qr_image = QRCodeFactory.generate_event_qr(event_name, start_date, end_date)
+    return send_file(qr_image, mimetype='image/png', as_attachment=False)
+
 
 @bp.route('/professor')
 @login_required
